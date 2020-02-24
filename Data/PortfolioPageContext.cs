df5ba91672d3d80d78939e9ba321206c15aded1f
Roshan Manuel <31125563+Roesh@users.noreply.github.com>
2020-02-24 13:43:51 -0500

    cleanup

--- Data/PortfolioPageContext.cs
+++ Data/PortfolioPageContext.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using PortfolioPage.Models;
+using System.Collections;
 
 namespace PortfolioPage.Data
 {
@@ -10,7 +12,28 @@ namespace PortfolioPage.Data
         {
         }
 
-        public DbSet<PortfolioPage.Models.Project> Project { get; set; }
-        //public DbSet<PortfolioPage.Models.ProjectStatusCat> ProjectStatusCat { get; set; }
+        public DbSet<project> project { get; set; }
+        public DbSet<projectCompletionDeadlineHistory> projectCompletionDeadlineHx { get; set; }
+        public DbSet<projectPhaseHistory> projectPhaseHistory { get; set; }
+
+        public DbSet<projectComponent> projectComponent { get; set; }
+        public DbSet<projectComponentCompletionDeadlineHistory> projectComponentCompletionDeadlineHx { get; set; }
+        public DbSet<projectUpdate> projectUpdate { get; set; }
+        public DbSet<projectUpdateEditHistory> projectUpdateEditHistory { get; set; }
+        
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<project>().ToTable("Project");
+            modelBuilder.Entity<projectPhaseHistory>().ToTable("Project Phase");
+            modelBuilder.Entity<projectCompletionDeadlineHistory>().ToTable("Project Completion Deadline history");
+
+            modelBuilder.Entity<projectComponent>().ToTable("Project Components");
+            modelBuilder.Entity<projectComponentCompletionDeadlineHistory>().ToTable("Project Component Deadline History");
+
+            modelBuilder.Entity<projectUpdate>().ToTable("Project Update");
+            modelBuilder.Entity<projectUpdateEditHistory>().ToTable("Project Update History");
+        }
     }
 }
